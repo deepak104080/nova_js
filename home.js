6823fabd340524a2b25bd33495dec434e03d871d
deepak104080 <deepak104080@gmail.com>
2023-01-03 01:08:03 +0530

    2nd Jan 2023 - DOM

---
+++ home.js
@@ -0,0 +1,45 @@
+console.log('------------------');
+
+
+// var doc = '<div><h2 id="heading">My First Web page</h2></div>';
+
+
+
+// var doc = {
+//     div: {
+//         h2: {
+//             id: 'heading',
+//             content: 'My First Web page'
+//         }
+//     }
+// }
+
+// doc.div.h2.id = 'new heading'
+
+
+
+// var document = {
+//     html: {
+//         head: {
+//             title : {
+//                 value: 'Title'
+//             },
+//             script : {
+//                 src: './home.js'
+//             }
+//         },
+//         body: {
+//             div: {
+//                 h2: {
+//                     id: 'heading',
+//                         content: 'My First Web page'
+//                     }
+//                 }
+//         }
+//     }
+// }
+
+
+//DOM Selector - getElementById
+//DOM Events - click, blur, focus
+//DOM Properties - innerHTML, height, width
